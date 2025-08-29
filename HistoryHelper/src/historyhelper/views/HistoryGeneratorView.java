79dd6104f10ec88d178fc9ddac9afc8e4309f8f6
Artem Belousov <88433071+spersics@users.noreply.github.com>
2025-08-29 13:19:15 +0300

    initial commit of HistoryHelper plugin

---
+++ HistoryHelper/src/historyhelper/views/HistoryGeneratorView.java
@@ -0,0 +1,44 @@
+package historyhelper.views;
+
+import historyhelper.ui.HistoryDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.jkiss.dbeaver.model.runtime.VoidProgressMonitor;
+import org.jkiss.dbeaver.model.struct.rdb.DBSTable;
+
+public class HistoryGeneratorView extends ViewPart {
+
+    @Override
+    public void createPartControl(Composite parent) {
+
+        Button btn = new Button(parent, SWT.PUSH);
+        btn.setText("Configure History");
+        btn.addListener(SWT.Selection, e -> openHistoryDialog());
+
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void openHistoryDialog() {
+        ISelection selection = getSite().getWorkbenchWindow().getSelectionService().getSelection();
+        if (selection instanceof IStructuredSelection) {
+            IStructuredSelection structured = (IStructuredSelection) selection;
+            Object firstElement = structured.getFirstElement();
+            if (firstElement instanceof DBSTable) {
+                DBSTable selectedTable = (DBSTable) firstElement;
+
+                HistoryDialog dialog = new HistoryDialog(getSite().getShell(), selectedTable, new VoidProgressMonitor());
+                dialog.open();
+            }
+        }
+    }
+
+}
